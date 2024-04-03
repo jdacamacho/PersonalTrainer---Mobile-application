b0b721015bed1ec988999e55fe994022ea010645
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-04-02 20:20:08 -0500

    Ejercicios basicos rutina

--- app/src/main/java/edu/unicauca/personaltrainer/MainActivity.kt
+++ app/src/main/java/edu/unicauca/personaltrainer/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -49,7 +52,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    form()
+                    routineExercisesBasic();
                 }
             }
         }
@@ -59,7 +62,7 @@ class MainActivity : ComponentActivity() {
 /*********** Inicio de Formulario de registro **********/
 
 @Composable
-fun header(modifier: Modifier = Modifier){
+fun headerForm(modifier: Modifier = Modifier){
     Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -86,8 +89,6 @@ fun header(modifier: Modifier = Modifier){
         )
     }
 }
-
-
 
 @Composable
 fun fieldForm(
@@ -163,14 +164,14 @@ fun fieldsFromForm() {
                 )
             }
             item{
-                registerButtom(onClick = {});
+                registerButtomForm(onClick = {});
             }
         }
     }
 }
 
 @Composable
-fun registerButtom(onClick: () -> Unit) {
+fun registerButtomForm(onClick: () -> Unit) {
     Button(
         onClick = onClick,
         modifier = Modifier
@@ -190,7 +191,7 @@ fun form(modifier: Modifier = Modifier){
     ) {
         Column(
         ){
-            header();
+            headerForm();
             fieldsFromForm();
         }
     }
@@ -202,6 +203,141 @@ fun PreviewForm(){
     form();
 }
 
+
 /*********** Fin de Formulario de registro **********/
 
-/*********** Inicio pagina de principal **********/
+/*********** Inicio pagina de rutinas **********/
+
+@Composable
+fun headerHomeRoutines(modifier: Modifier = Modifier){
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Text(
+            text = stringResource(R.string.header_routines),
+            modifier = modifier
+                .paddingFromBaseline(top = 30.dp, bottom = 8.dp)
+                .padding(start = 20.dp),
+            style = MaterialTheme.typography.titleLarge,
+            color = Color(0xFF1D4EA5)
+        )
+    }
+}
+@Composable
+fun cardExercise(
+    imageResource:Int,
+    kindExercise: String,
+    numberOfExercises: String,
+    modifier: Modifier = Modifier,
+    exercises: String
+){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxWidth()
+            .border(1.dp, Color(0xFF1D4EA5), shape = RoundedCornerShape(8.dp))
+    ){
+        Row (
+            modifier = Modifier
+                .padding(top = 10.dp)
+        ){
+            Column (
+                modifier = modifier
+                    .padding(15.dp)
+
+            ){
+                Text(text = kindExercise,
+                    color = Color(0xFF1D4EA5),
+                    style = MaterialTheme.typography.titleLarge)
+                Text(text = numberOfExercises)
+            }
+            Image(
+                painter = painterResource(imageResource),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = modifier
+                    .padding(top = 19.dp)
+                    .size(45.dp)
+                    .clip(CircleShape)
+                    .border(
+                        width = 2.dp,
+                        color = Color(0xFF1D4EA5),
+                        shape = CircleShape
+                    )
+            )
+        }
+        Column {
+            Text(text = exercises,
+                modifier = modifier
+                    .padding(bottom = 20.dp)
+            )
+        }
+        lookExercise(onClick = {});
+    }
+}
+
+@Composable
+fun lookExercise(onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .padding(bottom = 20.dp)
+            .height(40.dp),
+        colors = ButtonDefaults.buttonColors(Color(0xFF1D4EA5))
+    ) {
+        Text(text = stringResource(R.string.butto_look_routine))
+    }
+}
+
+@Composable
+fun routineCards() {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        items(4) { index ->
+            cardExercise(
+                imageResource = when (index) {
+                    0 -> R.drawable.brazos
+                    1 -> R.drawable.espalda
+                    2 -> R.drawable.pecho
+                    3 -> R.drawable.pierna
+                    else -> 0
+                },
+                kindExercise = when (index) {
+                    0 -> "Rutina de brazos"
+                    1 -> "Rutina de espalda"
+                    2 -> "Rutina de pecho"
+                    3 -> "Rutina de piernas"
+                    else -> ""
+                },
+                numberOfExercises = "4 ejercicios",
+                exercises = when (index){
+                    0 -> "Curl con mancuernas\nCurl con barra\nCurl martillo\nCurl Antebrazo"
+                    1 -> "Jalon al pecho\nRemo T\nRemo con barra\nJalon sentado"
+                    2 -> "Press banca plano\nPress banca inclinado\nPress con mancuernas\nFondos"
+                    3 -> "Sentadilla en Smith\nSentadilla Hack\nFemoral acostado\nPeso muerto con barra"
+                    else -> ""
+                }
+            )
+        }
+    }
+}
+@Composable
+fun routineExercisesBasic(){
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+        ){
+            headerHomeRoutines();
+            routineCards();
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun routineExercises(){
+    routineExercises();
+}
+
