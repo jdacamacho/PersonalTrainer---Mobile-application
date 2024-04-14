6e707a32329582185f48c2c76202366070f0554d
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-04-14 17:44:47 -0500

    lazy Column, falta cargar los recursos

--- app/src/main/java/edu/unicauca/personaltrainer/HomeExercise/lookExercise.kt
+++ app/src/main/java/edu/unicauca/personaltrainer/HomeExercise/lookExercise.kt
@@ -3,6 +3,7 @@ package edu.unicauca.personaltrainer.HomeExercise
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -71,7 +73,7 @@ fun ImageLookExercise(
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .padding(top = 40.dp, start = 20.dp, end = 20.dp, bottom = 40.dp)
+            .padding(top = 10.dp, start = 20.dp, end = 20.dp, bottom = 20.dp)
             .fillMaxWidth()
             .height(200.dp)
             .border(1.dp, Color(0xFF1D4EA5), shape = RoundedCornerShape(8.dp))
@@ -89,15 +91,15 @@ fun ImageLookExercise(
 @Composable
 fun FooterLookExercise(
     modifier: Modifier = Modifier,
-    kindExercise: String){
+    repeats: String){
     Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = kindExercise,
+            text = repeats,
             modifier = modifier
-                .paddingFromBaseline(top = 270.dp, bottom = 8.dp),
+                .paddingFromBaseline(top = 0.dp, bottom = 8.dp),
             style = MaterialTheme.typography.titleLarge,
             color = Color(0xFF1D4EA5)
         )
@@ -105,15 +107,51 @@ fun FooterLookExercise(
 }
 
 @Composable
-fun cardExerciseDetails(){
-    //HeaderLookExercise();
-    //ImageLookExercise();
-    //FooterLookExercise();
+fun cardExerciseDetails(
+    kindExercise: String,
+    imageResource: Int,
+    repeats: String
+){
+    HeaderLookExercise(kindExercise = kindExercise);
+    ImageLookExercise(imageResource = imageResource);
+    FooterLookExercise(repeats = repeats);
 }
 
+@Composable
+fun routineCardArms() {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        items(4) { index ->
+            cardExerciseDetails(
+                kindExercise = when (index) {
+                    0 -> "Hola"
+                    1 -> "Hola"
+                    2 -> ""
+                    3 -> ""
+                    else -> ""
+                },
+                imageResource = when (index) {
+                    0 -> R.drawable.brazos
+                    1 -> R.drawable.espalda
+                    2 -> R.drawable.pecho
+                    3 -> R.drawable.pierna
+                    else -> 0
+                },
+                repeats = when (index){
+                    0 -> "Curl con mancuernas\nCurl con barra\nCurl martillo\nCurl Antebrazo"
+                    1 -> "Jalon al pecho\nRemo T\nRemo con barra\nJalon sentado"
+                    2 -> "Press banca plano\nPress banca inclinado\nPress con mancuernas\nFondos"
+                    3 -> "Sentadilla en Smith\nSentadilla Hack\nFemoral acostado\nPeso muerto con barra"
+                    else -> ""
+                }
+            )
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun routineLookExercise(){
-    cardExerciseDetails();
+    routineCardArms();
 }
