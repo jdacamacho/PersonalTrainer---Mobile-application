45ea2db5a20885829609fb31bc5002c7c058e4ec
Simon Guzman <siguzman@unicauca.edu.co>
2024-04-28 15:10:55 -0500

    Pagina de login corregida, correcion en botones y elementos, agregacion de algunos strings

--- app/src/main/java/edu/unicauca/personaltrainer/MainActivity.kt
+++ app/src/main/java/edu/unicauca/personaltrainer/MainActivity.kt
@@ -77,91 +77,11 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
-
-
-
-/*********** Pagina de inicio de la aplicacion **********/
-@Composable
-fun OpenPage(modifier: Modifier = Modifier){
-    Surface (modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-        PersonalTrainerTheme {
-
-            Column(modifier = modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.SpaceEvenly,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Spacer(modifier = Modifier.height(64.dp))
-                Image(
-                    painter = painterResource(R.drawable.logopersonaltrainer),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(200.dp)
-                        .padding(8.dp)
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(
-                    text = stringResource(id = R.string.Title_Register),
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
-                Text(
-                    text = "Bienvenido a YOUR PERSONAL TRAINER",
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
-                Text(
-                    text = "La mejor aplicacion para tus entrenamientos",
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
-
-                Row(
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                {
-                    Column (modifier = modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ){
-                        Button(
-                            onClick = { /*TODO*/ },
-                            colors = ButtonDefaults.buttonColors(Color.Blue)
-
-                        ) {
-                            Text(text = "Iniciar Sesion")
-                        }
-                        Button(
-                            onClick = { /*TODO*/ },
-                            colors = ButtonDefaults.buttonColors(Color.Red)
-                        ) {
-                            Text(text = "Crear cuenta")
-                        }
-                        Button(
-                            onClick = { /*TODO*/ },
-                            colors = ButtonDefaults.buttonColors(Color.LightGray)
-                        ) {
-                            Text(text = "Continuar como anonimo", color = Color.Blue)
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-
 @Preview(showBackground = true)
 @Composable
 fun PreviewForm(){
     form();
 }
-
-
-/*********** Fin de Formulario de registro **********/
-
-
-/*********** Inicio pagina de principal **********/
 
 /*********** Inicio navegación **********/
 
@@ -246,95 +166,9 @@ fun PreviewNavegacion(){
             navigate = navigateAction::navigateTo)
     }
 }
-/*********** Inicio pagina de rutinas **********/
-@Composable
-fun OpenPagePreview(){
-    PersonalTrainerTheme {
-        OpenPage()
-    }
-}
 
 
-@Composable
-fun SignIn(modifier: Modifier = Modifier){
-    Surface (modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-        PersonalTrainerTheme {
-            Column(
-                modifier = modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.SpaceEvenly,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Spacer(modifier = Modifier.height(64.dp))
-                Image(
-                    painter = painterResource(R.drawable.logopersonaltrainer),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(200.dp)
-                        .padding(8.dp)
-                )
-
-                Text(
-                    text = stringResource(id = R.string.Title_Register),
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
-                Text(
-                    text = "Bienvenido a YOUR PERSONAL TRAINER",
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
-                Text(
-                    text = "La mejor aplicacion para tus entrenamientos",
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
-
-                // Campo de texto para el username
-                Row(
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                ) {
-                    Text(text = "Usuario: ")
-                    TextField(
-                        value = "Usuario",
-                        onValueChange = { /*TODO*/ },
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                }
-
-                // Campo de texto para la contraseña
-                Row(
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                ) {
-                    Text(text = "Contraseña: ")
-                    TextField(
-                        value = "Contraseña",
-                        onValueChange = { /*TODO*/ },
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                }
-
-                // Botón de inicio de sesión
-                Button(
-                    onClick = { /*TODO*/ },
-                    colors = ButtonDefaults.buttonColors(Color.Blue),
-                    modifier = Modifier.padding(top = 16.dp)
-                ) {
-                    Text(text = "Iniciar Sesion")
-                }
-
-                Spacer(modifier = Modifier.height(64.dp))
-            }
-        }
-    }
-}
 
 
-@Preview(showBackground = true)
-@Composable
-fun SignInPreview(){
-    PersonalTrainerTheme {
-        SignIn()
-    }
-}
 
 
